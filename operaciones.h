243bf82d3c15f5e1f15f7ad3a998f3edd288cde9
Braian Torres <brianntorress.s@gmail.com>
2025-06-18 00:54:33 -0300

    añadi archivo de cabecera donde se declaran funciones de operaciones logicas

---
+++ operaciones.h
@@ -0,0 +1,13 @@
+#ifndef OPERACIONES_H_INCLUDED
+#define OPERACIONES_H_INCLUDED
+
+#include <iostream>
+
+using namespace std;
+
+int calcularCombinacion(int dados[], int cantidadDados, int combinacion);
+int analizarCombinacion(int dados[], int cantidadDados);
+int obtenerPuntajeMaximo(int puntos[], int cantidadPuntos);
+void ordenarDados(int dados[], int cantidadDados);
+
+#endif // OPERACIONES_H_INCLUDED

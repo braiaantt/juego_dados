b45d154c2ef9cd64cb1e2f29caeb228686c13550
Braian Torres <brianntorress.s@gmail.com>
2025-06-18 18:15:46 -0300

    los modo de juegos devuelven el nombre del usuario ganador, le agregue parametros a la funcion puntuacion maxima y añadi declaracion de analizarInfoPartida

--- juego.h
+++ juego.h
@@ -1,13 +1,14 @@
 #ifndef JUEGO_H_INCLUDED
 #define JUEGO_H_INCLUDED
 
-void modoUnJugador();
-void modoDosJugadores();
-void puntuacionMaxima();
+std::string modoUnJugador(int infoPartida[]);
+std::string modoDosJugadores(int infoPartida[]);
+void puntuacionMaxima(std::string nombre, int infoMejorPartida[]);
 int jugarRonda();
 int tirarDados();
 int analizarCombinacion(int dados[], int cantidadDados);
 int calcularCombinacion(int dados[], int cantidadDados, int combinacion);
 int obtenerPuntajeMaximo(int puntos[], int cantidadPuntos);
+std::string analizarInfoPartida(std::string nombreActual, std::string nombreMejorJugador, int infoPartida[], int infoMejorPartida[]);
 
 #endif // JUEGO_H_INCLUDED

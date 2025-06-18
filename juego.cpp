1ff5be6018b029dddab0ec12f380feb35d9cf3b1
Braian Torres <brianntorress.s@gmail.com>
2025-06-18 18:17:23 -0300

    añadi desarrollo de funcion puntucionMaxima, las funciones de modo de juegos devuelven el nombre del usuario ganador y la funcion modoUnJugador manipula el vector infoPartida

--- juego.cpp
+++ juego.cpp
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-void modoUnJugador(){
+std::string modoUnJugador(int infoPartida[]){
 
-    int ESCALERA = 100, RESETEAR = -1;
+    int ESCALERA = 100, RESETEAR = -1, posRondas = 0, posPuntos = 1;
     std::string nombre = obtenerNombre();
     int puntaje = 0;
 
@@ -25,6 +25,10 @@ void modoUnJugador(){
 
             if (puntosDeRonda == ESCALERA){
                 mostrarJugadorGanaPorEscalera(nombre);
+
+                infoPartida[posRondas] = i+1;
+                infoPartida[posPuntos] = ESCALERA;
+
                 break;
             }
 
@@ -38,6 +42,10 @@ void modoUnJugador(){
             mostrarPuntajeDeRonda(i+1, puntosDeRonda);
 
             if(puntaje >= 100){
+
+                infoPartida[posRondas] = i+1;
+                infoPartida[posPuntos] = puntaje;
+
                 mostrarJugadorGana(nombre, puntaje, i+1);
                 break;
             }
@@ -57,6 +65,10 @@ void modoUnJugador(){
             int puntosDeRonda = jugarRonda();
 
             if (puntosDeRonda == ESCALERA){
+
+                infoPartida[posRondas] = ronda;
+                infoPartida[posPuntos] = ESCALERA;
+
                 mostrarJugadorGanaPorEscalera(nombre);
                 break;
             }
@@ -69,20 +81,27 @@ void modoUnJugador(){
             puntaje += puntosDeRonda;
 
             mostrarPuntajeDeRonda(ronda, puntosDeRonda);
-            ronda++;
 
             if(puntaje >= 100){
+
+                infoPartida[posRondas] = ronda;
+                infoPartida[posPuntos] = puntaje;
+
                 mostrarJugadorGana(nombre, puntaje, ronda);
                 break;
             }
+
+            ronda++;
 
         }
 
     }
 
+    return nombre;
+
 }
 
-void modoDosJugadores(){
+std::string modoDosJugadores(int infoPartida[]){
 
 
     string jugador1 = obtenerNombre();
@@ -101,9 +120,10 @@ void modoDosJugadores(){
             //Turno jugador 1
             mostrarRondaActual(jugador1, ronda, puntaje1);
             int puntos1 = jugarRonda();
+
             if (puntos1 == 100){
                 mostrarJugadorGanaPorEscalera(jugador1);
-                return;
+                return jugador1;
             }
             else if (puntos1 == -1){
                 puntaje1 = 0;
@@ -122,7 +142,7 @@ void modoDosJugadores(){
             int puntos2 = jugarRonda();
             if (puntos2 == 100){
                 mostrarJugadorGanaPorEscalera(jugador2);
-                return;
+                return jugador2;
             }
             else if (puntos2 == -1){
                 puntaje2 = 0;
@@ -149,7 +169,7 @@ void modoDosJugadores(){
 
             if (puntos1 == 100) {
                 mostrarJugadorGanaPorEscalera(jugador1);
-                return;
+                return jugador1;
             } else if (puntos1 == -1) {
                 puntaje1 = 0;
                 mostrarSeReseteanPuntos(jugador1);
@@ -157,7 +177,7 @@ void modoDosJugadores(){
                 puntaje1 += puntos1;
                 if (puntaje1 >= 100){
                     mostrarJugadorGana(jugador1, puntaje1, ronda);
-                    return;
+                    return jugador1;
                 }
             }
 
@@ -168,9 +188,10 @@ void modoDosJugadores(){
             //Turno jugador 2
             mostrarRondaActual(jugador2, ronda, puntaje2);
             int puntos2 = jugarRonda();
+
             if (puntos2 == 100){
                 mostrarJugadorGanaPorEscalera(jugador2);
-                return;
+                return jugador2;
             } else if (puntos2 == -1) {
                 puntaje2 = 0;
                 mostrarSeReseteanPuntos(jugador2);
@@ -178,7 +199,7 @@ void modoDosJugadores(){
                 puntaje2 += puntos2;
                 if (puntaje2 >= 100){
                     mostrarJugadorGana(jugador2, puntaje2, ronda);
-                    return;
+                    return jugador2;
                 }
             }
 
@@ -247,8 +268,26 @@ int tirarDados(){
 
 }
 
-void puntuacionMaxima(){
+void puntuacionMaxima(std::string nombre, int infoMejorJugada[]){
+    int posRondas = 0, posPuntos = 1;
+    cout<<endl;
+    cout<<"El mejor jugador fue: "<<nombre<<endl;
+    cout<<"Obtuvo un puntaje de "<<infoMejorJugada[posPuntos]<<" en "<<infoMejorJugada[posRondas]<<" rondas!"<<endl<<endl;
 
-    cout<<"Usted quiere ver la puntuacion maxima"<<endl;
+}
+
+std::string analizarInfoPartida(std::string nombreActual, std::string nombreJugador, int infoPartida[], int infoMejorPartida[]){
+
+    int posRondas = 0, posPuntos = 1;
+
+    if(nombreActual == "Sin registro" ||
+      (infoPartida[posPuntos] < infoMejorPartida[posPuntos] && infoPartida[posRondas] < infoMejorPartida[posRondas])){
+
+        infoMejorPartida[posRondas] = infoPartida[posRondas];
+        infoMejorPartida[posPuntos] = infoPartida[posPuntos];
+        return nombreJugador;
+    }
+
+    return nombreActual;
 
 }

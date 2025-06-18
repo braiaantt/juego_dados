206b38f809b1dfa08f08ed21b4eb11d1d78338cb
Braian Torres <brianntorress.s@gmail.com>
2025-06-18 00:49:51 -0300

    añadir las funciones mostrarPuntajeDeRonda, mostrarNumeroLanzamiento, mostrarPuntajeTirada y mostrarCombinacion

--- interfaz.cpp
+++ interfaz.cpp
@@ -61,3 +61,46 @@ void mostrarRondaActual(std::string nombre, int ronda, int puntaje){
     cout<<"Jugador: "<<nombre<<" | Ronda N°"<<ronda<<" | Puntaje: "<<puntaje<<endl;
 
 }
+
+void mostrarPuntajeDeRonda(int numRonda, int puntajeDeRonda){
+
+    cout<<endl;
+    cout<<"Tu puntaje de la ronda "<<numRonda<<" fue de: "<<puntajeDeRonda;
+    cout<<endl;
+
+    system("pause");
+    system("cls");
+
+}
+
+void mostrarNumeroLanzamiento(int numLanzamiento){
+
+    cout<<"-------"<<"Lanzamiento N°"<<numLanzamiento<<"-------"<<endl;
+
+}
+
+void mostrarPuntajeTirada(int puntajeTirada){
+
+    cout<<"Puntaje de la tirada: "<<puntajeTirada;
+
+    cout<<endl;
+    system("pause");
+    cout<<endl;
+
+}
+
+void mostrarCombinacion(int combinacion, int valorDado){
+
+    const int RESETEAR = -1;
+    const int SUMA = 1;
+    const int MULTIPLICAR = 2;
+    const int ESCALERA = 100;
+
+    switch(combinacion){
+        case ESCALERA:      cout<<"COMBINACION: Escalera"<<endl;                break;
+        case SUMA:          cout<<"COMBINACION: Suma de dados"<<endl;           break;
+        case RESETEAR:      cout<<"COMBINACION: Sexteto de 6"<<endl;            break;
+        case MULTIPLICAR:   cout<<"COMBINACION: Sexteto de "<<valorDado<<endl;  break;
+    }
+
+}

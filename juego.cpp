726f6c1cdee7f72b1ea3050ae7737f385fe7ed51
Braian Torres <brianntorress.s@gmail.com>
2025-06-18 14:44:45 -0300

    refactorizacion: movi la funcion modoDosJugadores() mas arriba

--- juego.cpp
+++ juego.cpp
@@ -54,60 +54,6 @@ void modoUnJugador(){
 
 }
 
-int jugarRonda(){
-
-    int ESCALERA = 100, RESETEAR = -1;
-    int lanzamientos = 3;
-    int puntos[lanzamientos];
-
-    for(int i = 0; i<lanzamientos; i++){
-
-        mostrarNumeroLanzamiento(i+1);
-
-        int puntajeTirada = tirarDados();
-
-        if(puntajeTirada == ESCALERA) return ESCALERA;
-        if(puntajeTirada == RESETEAR) return RESETEAR;
-
-        puntos[i] = puntajeTirada;
-
-        mostrarPuntajeTirada(puntajeTirada);
-
-    }
-
-    return obtenerPuntajeMaximo(puntos, lanzamientos);
-
-}
-
-int tirarDados(){
-
-    const int RESETEAR = -1;
-    const int SUMA = 1;
-    const int MULTIPLICAR = 2;
-    const int ESCALERA = 100;
-
-    int cantidadDados = 6, puntaje, combinacion;
-    int dados[cantidadDados];
-
-    for(int i = 0; i<6; i++){
-        dados[i] = (rand()%6)+1;
-    }
-
-    combinacion = analizarCombinacion(dados, cantidadDados);
-
-    mostrarDados(dados, cantidadDados);
-
-    switch(combinacion){
-        case RESETEAR:      mostrarCombinacion(RESETEAR, dados[0]);         puntaje = ESCALERA; break;
-        case SUMA:          mostrarCombinacion(SUMA, dados[0]);         puntaje = calcularCombinacion(dados, cantidadDados, SUMA); break;
-        case MULTIPLICAR:   mostrarCombinacion(MULTIPLICAR, dados[0]);  puntaje = RESETEAR; break;
-        case ESCALERA:      mostrarCombinacion(ESCALERA, dados[0]);     puntaje = calcularCombinacion(dados, cantidadDados, MULTIPLICAR); break;
-    }
-
-    return puntaje;
-
-}
-
 void modoDosJugadores(){
 
 
@@ -207,6 +153,60 @@ void modoDosJugadores(){
         }
 
     }
+
+}
+
+int jugarRonda(){
+
+    int ESCALERA = 100, RESETEAR = -1;
+    int lanzamientos = 3;
+    int puntos[lanzamientos];
+
+    for(int i = 0; i<lanzamientos; i++){
+
+        mostrarNumeroLanzamiento(i+1);
+
+        int puntajeTirada = tirarDados();
+
+        if(puntajeTirada == ESCALERA) return ESCALERA;
+        if(puntajeTirada == RESETEAR) return RESETEAR;
+
+        puntos[i] = puntajeTirada;
+
+        mostrarPuntajeTirada(puntajeTirada);
+
+    }
+
+    return obtenerPuntajeMaximo(puntos, lanzamientos);
+
+}
+
+int tirarDados(){
+
+    const int RESETEAR = -1;
+    const int SUMA = 1;
+    const int MULTIPLICAR = 2;
+    const int ESCALERA = 100;
+
+    int cantidadDados = 6, puntaje, combinacion;
+    int dados[cantidadDados];
+
+    for(int i = 0; i<6; i++){
+        dados[i] = (rand()%6)+1;
+    }
+
+    combinacion = analizarCombinacion(dados, cantidadDados);
+
+    mostrarDados(dados, cantidadDados);
+
+    switch(combinacion){
+        case RESETEAR:      mostrarCombinacion(RESETEAR, dados[0]);         puntaje = ESCALERA; break;
+        case SUMA:          mostrarCombinacion(SUMA, dados[0]);         puntaje = calcularCombinacion(dados, cantidadDados, SUMA); break;
+        case MULTIPLICAR:   mostrarCombinacion(MULTIPLICAR, dados[0]);  puntaje = RESETEAR; break;
+        case ESCALERA:      mostrarCombinacion(ESCALERA, dados[0]);     puntaje = calcularCombinacion(dados, cantidadDados, MULTIPLICAR); break;
+    }
+
+    return puntaje;
 
 }
 

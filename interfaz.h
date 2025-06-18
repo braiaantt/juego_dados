47a8ccaa4572ddf06c5e25590e04b54f72ddf08d
Braian Torres <brianntorress.s@gmail.com>
2025-06-18 00:50:55 -0300

    declarar funciones mostrarPuntajeDeRonda, mostrarNumeroLanzamiento, mostrarPuntajeTirada, mostrarCombinacion

--- interfaz.h
+++ interfaz.h
@@ -6,5 +6,9 @@ bool establecerRondas();
 int obtenerCantidadRondas();
 void mostrarDados(int dados[], int cantidadDados);
 void mostrarRondaActual(std::string nomre, int ronda, int puntaje);
+void mostrarPuntajeDeRonda(int numRonda, int puntajeDeRonda);
+void mostrarNumeroLanzamiento(int numLanzamiento);
+void mostrarPuntajeTirada(int puntajeTirada);
+void mostrarCombinacion(int combinacion, int valorDado);
 
 #endif // INTERFAZ_H_INCLUDED

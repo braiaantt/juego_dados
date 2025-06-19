dc9383d17d540b7f9a47efa01e9ee524ccb423e8
Braian Torres <brianntorress.s@gmail.com>
2025-06-19 20:44:53 -0300

    declarar funciones jugarTurno y chequearGanador

--- juego.h
+++ juego.h
@@ -10,5 +10,7 @@ int analizarCombinacion(int dados[], int cantidadDados);
 int calcularCombinacion(int dados[], int cantidadDados, int combinacion);
 int obtenerPuntajeMaximo(int puntos[], int cantidadPuntos);
 std::string analizarInfoPartida(std::string nombreActual, std::string nombreMejorJugador, int infoPartida[], int infoMejorPartida[]);
+int jugarTurno(int puntosActuales, int ronda);
+bool chequearGanador(int puntaje, int infoPartida[], int rondas);
 
 #endif // JUEGO_H_INCLUDED

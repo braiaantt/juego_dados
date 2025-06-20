06e2d7e4b19f05d267bfc853d2059536918cd34e
Braian Torres <brianntorress.s@gmail.com>
2025-06-20 15:53:30 -0300

    declarar funcion analizarInfoPartida

--- operaciones.h
+++ operaciones.h
@@ -9,5 +9,6 @@ int calcularCombinacion(int dados[], int cantidadDados, int combinacion);
 int analizarCombinacion(int dados[], int cantidadDados);
 int obtenerPuntajeMaximo(int puntos[], int cantidadPuntos);
 void ordenarDados(int dados[], int cantidadDados);
+std::string analizarInfoPartida(std::string nombreActual, std::string nombreMejorJugador, int infoPartida[], int infoMejorPartida[]);
 
 #endif // OPERACIONES_H_INCLUDED

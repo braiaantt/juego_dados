177efbde807d0e0c714e1461d36a18fb01b18adf
Braian Torres <brianntorress.s@gmail.com>
2025-06-20 15:57:10 -0300

    agregue logica para simularPartida

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "juego.h"
+#include "interfaz.h"
+#include "operaciones.h"
 
 using namespace std;
 
@@ -9,10 +11,11 @@ int main()
     srand(time(NULL));
     int seleccion, datos = 2;
     int infoMejorPartida[datos] = {1000,1000};
+    bool simularPartida = true;
 
     std::string nombreMejorJugador = "Sin ganador";
 
-    while(seleccion != 4){
+    while(seleccion != 5){
 
         std::string nombreJugador = "Sin ganador";
         int infoPartida[datos];
@@ -22,15 +25,16 @@ int main()
         cout<<"1. Un jugador"<<endl;
         cout<<"2. Dos jugadores"<<endl;
         cout<<"3. Maxima puntuacion"<<endl;
-        cout<<"4. Salir"<<endl;
+        cout<<"4. Simular partida"<<endl;
+        cout<<"5. Salir"<<endl;
         cin>>seleccion;
 
         switch(seleccion){
 
-            case 1: nombreJugador = modoUnJugador(infoPartida);              system("pause"); break;
-            case 2: nombreJugador = modoDosJugadores(infoPartida);           system("pause"); break;
-            case 3: puntuacionMaxima(nombreMejorJugador, infoMejorPartida);  system("pause"); break;
-            case 4: break;
+            case 1: nombreJugador = modoUnJugador(infoPartida, !simularPartida);    system("pause"); break;
+            case 2: nombreJugador = modoDosJugadores(infoPartida);                  system("pause"); break;
+            case 3: mostrarPuntuacionMaxima(nombreMejorJugador, infoMejorPartida);  system("pause"); break;
+            case 4: modoUnJugador(infoPartida, simularPartida);                     system("pause"); break;
 
         }
 
